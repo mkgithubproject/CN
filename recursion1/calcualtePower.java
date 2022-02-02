fa88eda69dba29559e0a19c9f940be4f72f40e7c
Mohit Kumar <37668348+mkgithubproject@users.noreply.github.com>
2022-02-02 17:23:29 +0530

    Update calcualtePower.java

--- recursion1/calcualtePower.java
+++ recursion1/calcualtePower.java
@@ -1,3 +1,9 @@
+/*Write a program to find x to the power n (i.e. x^n). Take x and n from the user. You need to return the answer.
+Do this recursively.
+Input format :
+Two integers x and n (separated by space)
+Output Format :
+x^n (i.e. x raise to the power n) */
 public class calcualtePower {
 
 	public static int power(int x, int n) {
