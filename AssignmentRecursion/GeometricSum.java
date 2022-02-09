abab131381e8f74cd9d3876cffe4b98e59ad9ef6
Mohit Kumar <37668348+mkgithubproject@users.noreply.github.com>
2022-02-09 17:50:46 +0530

    Create GeometricSum.java

---
+++ AssignmentRecursion/GeometricSum.java
@@ -0,0 +1,32 @@
+/*
+Geometric Sum
+Send Feedback
+Given k, find the geometric sum i.e.
+1 + 1/2 + 1/4 + 1/8 + ... + 1/(2^k) 
+using recursion.
+Input format :
+Integer k
+Output format :
+Geometric sum (upto 5 decimal places)
+Constraints :
+0 <= k <= 1000
+Sample Input 1 :
+3
+Sample Output 1 :
+1.87500
+Sample Input 2 :
+4
+Sample Output 2 :
+1.93750 */
+import java.lang.Math;
+public class GeometricSum {
+
+	public static double findGeometricSum(int k){
+		// Write your code here
+       if(k==0){
+           return 1;
+       }
+       double ans= findGeometricSum(k-1);
+        return ans+(1/Math.pow(2,k));
+	}
+}
