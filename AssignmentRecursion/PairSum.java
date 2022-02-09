fc0d4bc060518873b18186e519579357d720b249
Mohit Kumar <37668348+mkgithubproject@users.noreply.github.com>
2022-02-09 21:32:40 +0530

    Create PairSum.java

---
+++ AssignmentRecursion/PairSum.java
@@ -0,0 +1,38 @@
+/*
+Given a string S, compute recursively a new string where identical chars that are adjacent in the original string are separated from each other by a "*".
+Input format :
+String S
+Output format :
+Modified string
+Constraints :
+0 <= |S| <= 1000
+where |S| represents length of string S.
+Sample Input 1 :
+hello
+Sample Output 1:
+hel*lo
+Sample Input 2 :
+aaaa
+Sample Output 2 :
+a*a*a*a */
+
+
+public class PairSum {
+
+	// Return the updated string
+	public static String addStars(String s) {
+		// Write your code here
+        if(s.length()==1){
+            return s;
+        }
+        String ans=addStars(s.substring(1));
+        if(s.charAt(0)==ans.charAt(0)){
+            ans=s.charAt(0)+"*"+ans;
+        }
+        else{
+            ans=s.charAt(0)+ans;
+        }
+        return ans;
+
+	}
+}
