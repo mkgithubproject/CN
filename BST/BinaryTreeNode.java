7e06390a61f37ad000ebb07bc57dc5968d8a9a41
Mohit Kumar <37668348+mkgithubproject@users.noreply.github.com>
2022-07-19 09:14:04 +0530

    Create BinaryTreeNode.java

---
+++ BST/BinaryTreeNode.java
@@ -0,0 +1,11 @@
+class BinaryTreeNode<T> {
+      // instance variable
+      T data;
+      BinaryTreeNode<T> left;
+      BinaryTreeNode<T> right;
+      // constructor
+      public BinaryTreeNode(T data)
+      {
+       this.data = data;
+      }
+}
